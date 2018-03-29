8161d4ab3020c59d1c883b3c2cb9756ec117814c
Leticia Garcez Capovilla <leticiagc@unifei.edu.br>
2018-03-29 11:17:44 -0300

    Correct some bugs

--- iocBoot/iocrsfsl/runProcServ.sh
+++ iocBoot/iocrsfsl/runProcServ.sh
@@ -2,8 +2,8 @@
 
 # Use defaults is not set
 if [ -z "${RSFSL_DEVICE_TELNET_PORT}" ]; then
-    RSFSV_DEVICE_TELNET_PORT=20000
+    RSFSL_DEVICE_TELNET_PORT=20000
 fi
 
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n rsfsl${RSFSL_INSTANCE} -i ^C^D ${RSFSV_DEVICE_TELNET_PORT} ./runrsfsl.sh "$@"
+/usr/local/bin/procServ -f -n rsfsl${RSFSL_INSTANCE} -i ^C^D ${RSFSL_DEVICE_TELNET_PORT} ./runrsfsl.sh "$@"
